283f46936635905d4f723cd033267619346cff69
ljojua <ljojua@crocobet.com>
2025-07-02 11:58:18 +0400

    lesson 245

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {LayoutHeaderComponent} from '../../layouts/layout-header/layout-header.component';
 import {CatnavigationComponent} from '../../shared/catnavigation/catnavigation.component';
 import {NavBarComponent} from '../nav-bar/nav-bar.component';
+import {ProductsComponent} from '../../components/products/products.component';
 
 @Component({
   selector: 'estore-home',
   imports: [
     LayoutHeaderComponent,
     CatnavigationComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProductsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
